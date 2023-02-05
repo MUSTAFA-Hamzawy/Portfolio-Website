19524246d427b02ae19b0f08beec0a290df600b4
Mustafa Mahmoud <hamzawy2019579@gmail.com>
2023-02-05 18:30:04 +0200

    Starting...

---
+++ resources/views/admin/components/css.blade.php
@@ -0,0 +1,23 @@
+<!-- App favicon -->
+<link rel="shortcut icon" href="{{asset('backend')}}/assets/images/favicon.ico">
+
+<!-- jquery.vectormap css -->
+<link href="{{asset('backend')}}/assets/libs/admin-resources/jquery.vectormap/jquery-jvectormap-1.2.2.css"
+      rel="stylesheet"
+      type="text/css" />
+
+<!-- DataTables -->
+<link href="{{asset('backend')}}/assets/libs/datatables.net-bs4/css/dataTables.bootstrap4.min.css" rel="stylesheet"
+      type="text/css" />
+
+<!-- Responsive datatable examples -->
+<link href="{{asset('backend')}}/assets/libs/datatables.net-responsive-bs4/css/responsive.bootstrap4.min.css"
+      rel="stylesheet"
+      type="text/css" />
+
+<!-- Bootstrap Css -->
+<link href="{{asset('backend')}}/assets/css/bootstrap.min.css" id="bootstrap-style" rel="stylesheet" type="text/css" />
+<!-- Icons Css -->
+<link href="{{asset('backend')}}/assets/css/icons.min.css" rel="stylesheet" type="text/css" />
+<!-- App Css-->
+<link href="{{asset('backend')}}/assets/css/app.min.css" id="app-style" rel="stylesheet" type="text/css" />
