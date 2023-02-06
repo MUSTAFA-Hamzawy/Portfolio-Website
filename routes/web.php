d28d7fc0f72ea4a5135dea46a1d06c07799df139
Mustafa Mahmoud <hamzawy2019579@gmail.com>
2023-02-06 22:08:40 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+
+    return view('frontend.pages.index');
 });
 
 Route::get('/dashboard', function () {
