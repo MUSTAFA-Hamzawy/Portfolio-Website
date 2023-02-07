0af154daf06a62de5dea236ba8dfac55f3230eaf
Mustafa Mahmoud <hamzawy2019579@gmail.com>
2023-02-07 20:28:04 +0200

    Create aboutController.php

---
+++ app/Http/Controllers/aboutController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\aboutRequest;
+use App\Models\aboutModel;
+use App\MyHelpers;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Http\Request;
+
+class aboutController extends Controller
+{
+    public function aboutShow(){
+        try {
+            $data = aboutModel::all()->first();
+            return view('admin.about.default_about_view', compact('data'));
+        }catch (ModelNotFoundException $exp){
+            toastr()->error('Failed to fetch the data.');
+            return redirect()->back();
+        }
+    }
+
+    public function aboutUpdate(aboutRequest $request){
+        $data = $request->validated();
+
+        // moving the image
+        $requestHasImage = $request->file('about_image');
+        if ($requestHasImage){
+            $data['about_image'] = MyHelpers::uploadImage($request->file('about_image'), 'uploads/images/about');
+        }
+
+        // update
+        try {
+
+            $updatedData = [
+                'title' => $data['title'],
+                'sub_title' => $data['sub_title'],
+                'description' => $data['about_description']
+            ];
+            $requestHasImage ? $updatedData['about_image'] = $data['about_image'] : null;
+
+            aboutModel::findOrFail($request->id)->update($updatedData);
+            return response(['msg' => 'Updated Successfully'], 200);
+        }catch (ModelNotFoundException $exp){
+            toastr()->error('Failed to update.');
+            return redirect()->back();
+        }
+    }
+}
