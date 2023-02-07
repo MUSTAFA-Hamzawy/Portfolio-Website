698a36d705369591d54524a1bcbad767dc187f4c
Mustafa Mahmoud <hamzawy2019579@gmail.com>
2023-02-07 18:37:30 +0200

    Created the views files for the banner section

---
+++ resources/views/frontend/pages/home_banner.blade.php
@@ -0,0 +1,29 @@
+@php use App\Models\homeBannerModel; @endphp
+@php
+$data = homeBannerModel::all()->first();
+@endphp
+
+<section class="banner">
+    <div class="container custom-container">
+        <div class="row align-items-center justify-content-center justify-content-lg-between">
+            <div class="col-lg-6 order-0 order-lg-2">
+                <div class="banner__img text-center text-xxl-end">
+                    <img src="{{url('uploads/images/home_banner/' . $data->banner_image)}}" alt="">
+                </div>
+            </div>
+            <div class="col-xl-5 col-lg-6">
+                <div class="banner__content">
+                    <h2 class="title wow fadeInUp" data-wow-delay=".2s"><span>{{$data->title}}</h2>
+                    <p class="wow fadeInUp" data-wow-delay=".4s">{{$data->bio_description}}</p>
+                    <a href="about.html" class="btn banner__btn wow fadeInUp" data-wow-delay=".6s">more about me</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="scroll__down">
+        <a href="#aboutSection" class="scroll__link">Scroll down</a>
+    </div>
+    <div class="banner__video">
+        <a href="{{$data->video_url}}" class="popup-video"><i class="fas fa-play"></i></a>
+
+</section>
