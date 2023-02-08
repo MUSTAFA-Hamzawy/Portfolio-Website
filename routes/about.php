2a0943d9cb052b4ff4680328c6096e78a211f078
Mustafa Mahmoud <hamzawy2019579@gmail.com>
2023-02-08 13:35:10 +0200

    Update about.php

--- routes/about.php
+++ routes/about.php
@@ -2,9 +2,16 @@
 
 
 use App\Http\Controllers\aboutController;
+use App\Models\aboutModel;
 use Illuminate\Support\Facades\Route;
 
+// back-end routes
 Route::middleware(['auth', 'verified'])->prefix('admin')->controller(aboutController::class)->group(function (){
     Route::get('about', 'aboutShow')->name('about-show');
     Route::post('about_update', 'aboutUpdate')->name('about-update');
+});
+
+// front-end routes
+Route::controller(aboutController::class)->group(function (){
+    Route::view('/about', 'frontend.pages.about_full_page', ['data' => aboutModel::all()->first()])->name('about-full-page');
 });
