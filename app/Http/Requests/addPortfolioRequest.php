ae86b87591ecb66471fdc24109d86b844dc21b86
Mustafa Mahmoud <hamzawy2019579@gmail.com>
2023-02-09 15:47:30 +0200

    requests for the portfolio

---
+++ app/Http/Requests/addPortfolioRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class addPortfolioRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return 1;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        $allowedExtensions = 'jpg,png,webp,jpeg,gif';
+
+        return [
+            'title' => ['required', 'string', 'max:150'],
+            'category' => ['required', 'int'],
+            'project_description' => ['required'],
+            'image' => [ 'image', 'mimes:' . $allowedExtensions, 'max:4096']
+        ];
+    }
+}
